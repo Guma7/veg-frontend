7f5a480314f6cd9f87116e779020b9fa2123d24c
Guma7 <gumascott7@gmail.com>
2025-06-22 19:30:03 -0300

    Deploypictures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,19 @@ import styled from 'styled-components'
 import { FeaturedRecipes } from '../components/home/FeaturedRecipes'
 
 const Hero = styled.div`
-  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-              url('/nature-basket-apples-grapes-wallpaper-preview.jpg') center/cover;
+  background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.7)),
+              url('/frut.jpg') center/cover;
   height: 60vh;
   display: flex;
   align-items: center;
   justify-content: center;
   text-align: center;
-  color: white;
+  color: rgba(255, 255, 255, 0.95);
   padding: ${props => props.theme.spacing.xl};
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  filter: contrast(1.1) brightness(0.9);
 `
 
 const HeroContent = styled.div`
@@ -20,13 +24,19 @@ const HeroContent = styled.div`
 `
 
 const HeroTitle = styled.h1`
-  font-size: ${props => props.theme.fonts.sizes.xxl};
+  font-size: ${props => props.theme.fonts.sizes['2xl']};
   margin-bottom: ${props => props.theme.spacing.lg};
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3);
+  font-weight: ${props => props.theme.fonts.weights.bold};
+  color: rgba(255, 255, 255, 0.98);
 `
 
 const HeroText = styled.p`
   font-size: ${props => props.theme.fonts.sizes.lg};
   margin-bottom: ${props => props.theme.spacing.xl};
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.2);
+  font-weight: ${props => props.theme.fonts.weights.medium};
+  color: rgba(255, 255, 255, 0.92);
 `
 
 export default function HomePage() {
