e09d2542140256cae0744ab462cab7122894e584
Guma7 <gumascott7@gmail.com>
2025-07-06 01:29:58 -0300

    Descrição da mudança

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import { FeaturedRecipes } from '../components/home/FeaturedRecipes'
 
 const Hero = styled.div`
+  background-color: #000000;
   background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.7)),
               url('/frut.jpg') center/cover;
   height: 60vh;
@@ -16,6 +17,8 @@ const Hero = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+  background-attachment: scroll;
+  will-change: transform;
   filter: contrast(1.1) brightness(0.9);
 `
 
@@ -42,6 +45,7 @@ const HeroText = styled.p`
 export default function HomePage() {
   return (
     <>
+      <link rel="preload" as="image" href="/frut.jpg" />
       <Hero>
         <HeroContent>
           <HeroTitle>Descubra o Mundo da Culinária Vegana</HeroTitle>
